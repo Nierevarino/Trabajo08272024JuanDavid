ddcfd0f9b24790afbd063ffb1fa9b3eb46ead9fe
Nierevarino <nierevarino2006@gmail.com>
2024-08-28 18:18:29 -0500

    Tercer commit

--- Punto3.cs
+++ Punto3.cs
@@ -5,7 +5,52 @@ public class Punto3
     {
         for (int multi = 1; multi <= 10; multi++)
         {
+            Console.WriteLine($"Multiplicacion entre 1 por {multi} = {1*multi}");
+        }
+
+        for (int multi = 1; multi <= 10; multi++)
+        {
+            Console.WriteLine($"Multiplicacion entre 2 por {multi} = {2*multi}");
+        }
+
+        for (int multi = 1; multi <= 10; multi++)
+        {
             Console.WriteLine($"Multiplicacion entre 3 por {multi} = {3*multi}");
+        }
+
+        for (int multi = 1; multi <= 10; multi++)
+        {
+            Console.WriteLine($"Multiplicacion entre 4 por {multi} = {4*multi}");
+        }
+
+        for (int multi = 1; multi <= 10; multi++)
+        {
+            Console.WriteLine($"Multiplicacion entre 5 por {multi} = {5*multi}");
+        }
+
+        for (int multi = 1; multi <= 10; multi++)
+        {
+            Console.WriteLine($"Multiplicacion entre 6 por {multi} = {6*multi}");
+        }
+
+        for (int multi = 1; multi <= 10; multi++)
+        {
+            Console.WriteLine($"Multiplicacion entre 7 por {multi} = {7*multi}");
+        }
+
+        for (int multi = 1; multi <= 10; multi++)
+        {
+            Console.WriteLine($"Multiplicacion entre 8 por {multi} = {8*multi}");
+        }
+
+        for (int multi = 1; multi <= 10; multi++)
+        {
+            Console.WriteLine($"Multiplicacion entre 9 por {multi} = {9*multi}");
+        }
+
+        for (int multi = 1; multi <= 10; multi++)
+        {
+            Console.WriteLine($"Multiplicacion entre 10 por {multi} = {10*multi}");
         }
     }
 }
